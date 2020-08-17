77cc103aa2566b3c65a6c99de0fe9fa000b17cf8
Bruna <brunasgf@gmail.com>
2020-08-16 22:01:55 -0300

    refatorando controller para carrossel

--- api/controller/githubCtrl.js
+++ api/controller/githubCtrl.js
@@ -43,49 +43,44 @@ class GitHubCtrl {
     }
 
     getInRepos(query) {
-        let repos = []
+
         const defaultRepo = {
-            id: 0,
-            name: null,
-            description: null
+            id: id + 1,
+            type: "application/vnd.lime.collection+json",
+            to: "1042221589186385@messenger.gw.msging.net",
+            content: {
+                itemType: "application/vnd.lime.document-select+json",
+                items: []
+            }
         }
 
-        //console.log(this.repos)
-        //console.log("\n\n\n\n\n\n\n")
+        const defaultItem = {
+            header: {
+                type: "application/vnd.lime.media-link+json",
+                value: {
+                    title: null,
+                    text: null,
+                    type: "image/jpeg",
+                    uri: "https://avatars1.githubusercontent.com/u/4369522?s=200&v=4"
+                }
+            }
+        }
+
+        const repo = _.clone(defaultRepo)
 
         for (let i = 0; i < this.repos.length; i++) {
-            const repo = _.clone(defaultRepo)
+            const item = _.clone(defaultItem)
             if (
                 (this.repos[i].name !== null && this.repos[i].name.indexOf(query) !== -1) ||
                 (this.repos[i].description !== null && this.repos[i].description.indexOf(query) !== -1)
             ) {
-                repo.name = this.repos[i].name
-                repo.description = this.repos[i].description
-                repo.id = this.id + 1
-                repos = [{           
-                    id:repo.id,
-                    to: "1042221589186385@messenger.gw.msging.net",
-                    type: "application/vnd.lime.collection+json",
-                    content: [{
-                            itemType: "application/vnd.lime.document-select+json", 
-                            items: [{
-                                header: [{
-                                    type: "application/vnd.lime.media-link+json", 
-                                    value: [{
-                                        title: repo.name, 
-                                        text: repo.description,
-                                        type: "image/jpeg", 
-                                        uri: "https://avatars1.githubusercontent.com/u/4369522?s=200&v=4"
-                                    }]  
-                                }]
-                            }]
-                        }]
-                    }]
-                repos.push(repo)
+                item.header.title = this.repos[i].name
+                item.header.text = this.repos[i].description
+
+                repo.content.items.push(item)
             }
         }
-        JSON.stringify(repos)
-        return repos
+        return repo
     }
 }
 
